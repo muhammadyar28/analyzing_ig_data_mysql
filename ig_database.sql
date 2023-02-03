a4d46a5ca4779b8c4f5d23f5b1f69811759e2313
Muhammad Asfand Yar <124397190+muhammadyar28@users.noreply.github.com>
2023-02-03 16:49:48 -0500

    Update ig_database.sql

--- ig_database.sql
+++ ig_database.sql
@@ -7,8 +7,8 @@ USE ig_database;
 
 CREATE TABLE users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+    	username VARCHAR(255) UNIQUE NOT NULL,
+    	created_at TIMESTAMP DEFAULT NOW()
     );
 
 -- PHOTOS Schema
@@ -18,11 +18,11 @@ CREATE TABLE users (
 -- user_id also cannot be NULL, their must be an existing user that posted that image
 
 CREATE TABLE photos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    image_url VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES users (id)
+    	id INT AUTO_INCREMENT PRIMARY KEY,
+    	image_url VARCHAR(255) NOT NULL,
+    	user_id INT NOT NULL,
+    	created_at TIMESTAMP DEFAULT NOW(),
+    	FOREIGN KEY (user_id) REFERENCES users (id)
 );
  
 -- USERS Schema
@@ -33,12 +33,12 @@ CREATE TABLE photos (
 
 CREATE TABLE comments (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    comment_text VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL,
-    photo_id INTEGER NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY (photo_id) REFERENCES photos(id)
+    	comment_text VARCHAR(255) NOT NULL,
+    	user_id INTEGER NOT NULL,
+    	photo_id INTEGER NOT NULL,
+    	created_at TIMESTAMP DEFAULT NOW(),
+    	FOREIGN KEY(user_id) REFERENCES users(id),
+    	FOREIGN KEY (photo_id) REFERENCES photos(id)
     );
     
 -- LIKES Schema
@@ -49,11 +49,11 @@ CREATE TABLE comments (
 
 CREATE TABLE likes (
 	user_id INT NOT NULL,
-    photo_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(user_id) REFERENCES users(id),
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
-    PRIMARY KEY(user_id, photo_id)
+    	photo_id INT NOT NULL,
+    	created_at TIMESTAMP DEFAULT NOW(),
+    	FOREIGN KEY(user_id) REFERENCES users(id),
+    	FOREIGN KEY(photo_id) REFERENCES photos(id),
+    	PRIMARY KEY(user_id, photo_id)
     );
     
 -- FOLLOWERS Schema
@@ -64,11 +64,11 @@ CREATE TABLE likes (
 
 CREATE TABLE followers (
 	follower_id INT NOT NULL,
-    followee_id INT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY(follower_id) REFERENCES users(id),
-    FOREIGN KEY(followee_id) REFERENCES users(id),
-    PRIMARY KEY(followee_id, follower_id)
+    	followee_id INT NOT NULL,
+    	created_at TIMESTAMP DEFAULT NOW(),
+    	FOREIGN KEY(follower_id) REFERENCES users(id),
+    	FOREIGN KEY(followee_id) REFERENCES users(id),
+    	PRIMARY KEY(followee_id, follower_id)
     );
 
 -- TAGS Scheme
@@ -88,16 +88,16 @@ CREATE TABLE followers (
 
 CREATE TABLE tags (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    tag_name VARCHAR(255) UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
+    	tag_name VARCHAR(255) UNIQUE,
+    	created_at TIMESTAMP DEFAULT NOW()
     );
     
 CREATE TABLE photo_tags (
 	photo_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
-    FOREIGN KEY(tag_id) REFERENCES tags(id),
-    PRIMARY KEY(photo_id, tag_id)
+    	tag_id INT NOT NULL,
+    	FOREIGN KEY(photo_id) REFERENCES photos(id),
+    	FOREIGN KEY(tag_id) REFERENCES tags(id),
+    	PRIMARY KEY(photo_id, tag_id)
     );
 
 
